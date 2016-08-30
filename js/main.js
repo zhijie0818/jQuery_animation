9978d15226c9d8f5c371dc867c97c803f51f6831
zhijie <zhijie@zwoil.com>
2016-08-30 16:39:29 +0800

    jquery——mobile

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+// $(document).scroll(function(){
+// 	     var scroll_top = $(document).scrollTop();
+//         //
+//         var div1_top = $('#section1').offset().top;
+//         var div1_height = $('#section1').height();
+//         console.log(scroll_top);
+//         console.log(div1_top);
+//         console.log(div1_height);
+
+//         if(scroll_top >= div1_top-div1_height && scroll_top<div1_top && flage==true ){
+//         		$(".intro1 img").fadeIn(3000);
+//         }
+// })
